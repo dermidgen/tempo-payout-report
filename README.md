cde19c324a4455fe0c106e0288f5131c67dc37a1
Luis Gomez <lgomez@gmail.com>
2017-05-31 12:45:58 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# How Much Gun?
+
+## 1. Create a `.env` file.
+
+```shell
+# Your Jira username
+USER=
+# Your Jira password
+PASS=
+# Your Gun.io hourly rate
+RATE=
+```
+
+## 2. Run `npm i`
+
+## 3. Run `npm run report`
---
+++ index.js
@@ -0,0 +1,54 @@
+const util = require('util')
+const config = require('dotenv').config().parsed
+const request = require('request')
+const moment = require('moment')
+const numeral = require('numeral')
+
+const domain = config.DOMAIN
+const user = config.USER
+const username = user
+const pass = config.PASS
+const rate = Number(config.RATE)
+
+const api = request.defaults({
+  baseUrl: `https://${domain}/rest`,
+  json: true,
+  auth: { user, pass, sendImmediately: true }
+})
+
+const report = (week = 0) => {
+  return new Promise((resolve, reject) => {
+    const qs = {
+      dateFrom: moment().subtract(week, 'weeks').day(-1).format('YYYY-MM-DD'),
+      dateTo: moment().subtract(week, 'weeks').day(+5).format('YYYY-MM-DD'),
+      username
+    }
+
+    api.get('/tempo-timesheets/3/worklogs/', { qs }, (err, res, json) => {
+      if (err) throw err
+      const seconds = json.reduce((seconds, work) => {
+        return seconds + work.billedSeconds
+      }, 0)
+      const hours = ((seconds/60)/60)
+      const dolars = numeral(hours*rate).format('$0,0.00')
+      const totals = { seconds, hours, dolars }
+      resolve({
+        from: qs.dateFrom,
+        to: qs.dateTo,
+        total: dolars
+      })
+    })
+  })
+}
+
+Promise.all([
+  report(3),
+  report(2),
+  report(1),
+  report(0)
+]).then(results => {
+  results.sort((a,b) => {
+    return new Date(a.from).getTime() - new Date(b.from).getTime()
+  })
+  console.dir(results, { colors: true, depth: null })
+})
